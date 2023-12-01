36eb626a9b688f9096e7ad22ffa9c8e15c68dbf9
Robin De Schepper <robin.deschepper93@gmail.com>
2023-12-01 10:24:44 +0100

    b0

--- bsb_arbor/__init__.py
+++ bsb_arbor/__init__.py
@@ -7,5 +7,5 @@ from .simulation import ArborSimulation
 from .adapter import ArborAdapter
 from . import devices
 
-__version__ = "0.0.0a0"
+__version__ = "0.0.0b0"
 __plugin__ = SimulationBackendPlugin(Simulation=ArborSimulation, Adapter=ArborAdapter)
