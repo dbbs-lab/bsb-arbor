30c584c95e241aa0f9428402e500ed11a2aa5236
Helveg <robin.deschepper93@gmail.com>
2024-03-28 12:44:23 +0100

    Bump version: 0.0.0-b2 → 0.0.0-b3

--- bsb_arbor/__init__.py
+++ bsb_arbor/__init__.py
@@ -8,5 +8,5 @@ from . import devices
 from .adapter import ArborAdapter
 from .simulation import ArborSimulation
 
-__version__ = "0.0.0-b2"
+__version__ = "0.0.0-b3"
 __plugin__ = SimulationBackendPlugin(Simulation=ArborSimulation, Adapter=ArborAdapter)
