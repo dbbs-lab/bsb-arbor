2f81e2ab9173b86b20594bc0d85a36bc9245bc10
drodarie <drodarie@users.noreply.github.com>
2025-01-07 16:45:41 +0000

    docs: bump version: v4.1.0 → v4.1.1 [skip ci]

--- bsb_arbor/__init__.py
+++ bsb_arbor/__init__.py
@@ -8,5 +8,5 @@ from . import devices
 from .adapter import ArborAdapter
 from .simulation import ArborSimulation
 
-__version__ = "4.1.0"
+__version__ = "4.1.1"
 __plugin__ = SimulationBackendPlugin(Simulation=ArborSimulation, Adapter=ArborAdapter)
